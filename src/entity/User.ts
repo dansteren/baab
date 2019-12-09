51f4626eac7ad89352f71fcfd1a40a13829be34f
Dan Steren <daniel.d.steren@gmail.com>
2019-12-08 22:24:41 -0700

    Add graphql decorators to User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,17 +1,23 @@
 import { Entity, PrimaryGeneratedColumn, Generated, Column, BaseEntity } from "typeorm";
+import { Field, Int, ObjectType } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
   @Column()
   @Generated('uuid')
   uuid!: string;
 
+  @Field()
   @Column()
   firstName!: string;
 
+  @Field()
   @Column()
   lastName!: string;
 }
