33f7a2752e4a09f0aa55298ed08a237d4d3d36bc
Dan Steren <daniel.d.steren@gmail.com>
2019-12-26 18:28:53 -0700

    Read typeorm host/port from env variables

--- ormconfig.js
+++ ormconfig.js
@@ -13,8 +13,8 @@ switch (process.env.NODE_ENV) {
 
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: 'baab',
